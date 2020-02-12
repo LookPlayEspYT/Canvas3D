196ac7e24443da1210cbb45fb5dcf4839ca415e7
LookPlayEsp <60762375+LookPlayEspYT@users.noreply.github.com>
2020-02-11 18:29:58 -0700

    Update 0.7V
    add new features & there will be more

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
 var sc=new screen("canvas"),
+	sc2=new screen("canvas2"),
 	sc_visible=true,
-	cb1=new cube(50,50,sc),
-	point=new fuguePoint(150,75);
+	cb1=new cube(50,50,20,sc),
+	point=new fuguePoint(150,75),
+	keys=new keysMapping();
 
+sc.setup();
+sc.clear();
 sc.border("solid");
+sc.size("80%","");
 sc.updtPoint(point);
 sc.updtCube(cb1);
+sc.error("Hola","K<br>Hace");
+sc2.setup();
+sc2.clear();
+sc2.border("solid");
+sc2.size("80%","");
+sc2.updtPoint(point);
+sc2.updtCube(cb1);
+sc2.error("Hola","K<br>Hace");
+keys.setup();
 
-function toggle(){
-	if (sc_visible) {
-		sc.hidden();
-	} else {
-		sc.show();
-	};
-	sc_visible=!sc_visible;
+setInterval(() => {
+	if (keys.keyAscii != 0) {
+		press(keys.keyChar);
+	}
+},1000/50);
+
+function press(_e_){
+	cb1.move(cb1.x[0]+2,cb1.y[0]+1,20);
+	sc.clear();
+	sc.updtCube(cb1);
+	console.log(_e_);
+}
+function release(_e__){
+	console.log("----------------------");
 }
 
 console.log(sc);
